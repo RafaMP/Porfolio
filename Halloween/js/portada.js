5eb1225e325e63692a07b273abcecadfff9b67d7
RafaMP <151637040+RafaMP@users.noreply.github.com>
2023-11-23 19:42:05 +0100

    Add files via upload

---
+++ Halloween/js/portada.js
@@ -0,0 +1,26 @@
+/***********************************************************************************/
+/*                        Hoja de estilo de Halloween.html                         */
+/*                        Author: Rafael Moncayo                                   */
+/*                        version: 1.0                                             */
+/*                        fecha: noviembre 2023                                    */
+/*                        Comments: ejercicio para porfolio (2)                    */
+/***********************************************************************************/
+
+
+function show(element){
+    document.getElementById(element).style.visibility="visible";
+    /** Extrae (get) un elemento (bats) y cambia algo en su estilo (visibilidad) **/
+}
+
+function hide(element){
+    document.getElementById(element).style.visibility="hidden";
+    /** Extrae (get) un elemento (bats) y cambia algo en su estilo (visibilidad) **/
+}
+
+function sound(element){
+    document.getElementById(element).play();
+}
+
+function pause(element){
+    document.getElementById(element).pause();
+}
